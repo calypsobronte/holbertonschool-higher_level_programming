8756f61439614216bd57e39a2dd505b667ffd032
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-10 21:36:08 +0000

    add task 8. To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def uppercase(str):
+    str = str + "\n"
+    for i in str:
+        letter = "abcdefghijklmnopqrstuvwxyz"
+        if i in letter:
+            letter = ord(i) - 32
+        else:
+            letter = ord(i)
+        print("{:c}".format(letter), end='')
