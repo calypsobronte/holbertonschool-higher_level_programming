074ea6f729c3a5263abae769b335555ffa3f5fd6
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-10 00:58:22 +0000

    Fix change temp2 for list Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,21 +9,21 @@ int check_cycle(listint_t *list)
 listint_t *temp1, *temp2;
 
 temp1 = list;
-temp2 = list->next;
+temp2 = list;
 
 if (list == NULL || list->next == NULL)
 return (0);
 while (temp1 != NULL && temp2->next != NULL && temp2 != NULL)
 {
-if (temp1 == temp2)
-{
-return (1);
-}
 temp1 = temp1->next;
 if (temp2->next->next != NULL)
 temp2 = temp2->next->next;
 else
 break;
+if (temp1 == temp2)
+{
+return (1);
+}
 }
 return (0);
 }
