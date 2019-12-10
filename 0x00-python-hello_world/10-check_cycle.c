48e83ddd1088125b459393a49f3bb36c1134b68d
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-10 00:32:12 +0000

    add 10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,24 @@
-#script
+#include "lists.h"
+/**
+ * check_cycle - function check list
+ * @list: list
+ * Return: 0
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *temp1, *temp2;
+
+temp1 = list;
+temp2 = temp1[0].next;
+
+if (list == NULL && list[0].next == NULL)
+return (0);
+while (temp1 != NULL && temp2[0].next != NULL && temp2[0].next->next != NULL)
+{
+if (temp1 == temp2)
+return (1);
+temp1 = temp1[0].next;
+temp2 = temp2[0].next->next;
+}
+return (0);
+}
