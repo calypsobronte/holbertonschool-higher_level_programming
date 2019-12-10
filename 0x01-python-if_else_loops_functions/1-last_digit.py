e99fe855f2703ba1e40035b067fae2ebf3abd19e
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-10 19:27:37 +0000

    Fix task 1 salto de linea

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,11 +9,11 @@ if number < 0:
 else:
     last = positive % 10
 if last > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5\n"
+    print("Last digit of {:d} is {:d} and is greater than 5"
           .format(number, last))
 elif (last < 0 and last < 5) or (last <= 5 and last != 0):
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0\n"
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
           .format(number, last))
 elif last == 0:
-    print("Last digit of {:d} is {:d} and is 0\n"
+    print("Last digit of {:d} is {:d} and is 0"
           .format(number, last))
