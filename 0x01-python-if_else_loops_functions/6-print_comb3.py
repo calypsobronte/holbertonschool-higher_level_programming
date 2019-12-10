11610f6e3766f2a1546cc20f030332eeddc77511
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-10 20:44:52 +0000

    add task [200~7. islower~

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+for i in range(0, 100):
+    for c in range(i + 1, 100):
+        if i == 56 and c == 57:
+            print("{:02d}, ".format(i))
+            print("{:02d}, ".format(c))
+        else:
+            print("{:d}, ".format(i))
+            print("{:d}, ".format(c))
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def islower(c):
+    letter = ord(c)
+    if letter >= 97 and letter <= 127:
+        return (True)
+    else:
+        return (False)
