a3e580deeba5324ffe8587b4a5647ffc334283bd
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-10 21:09:22 +0000

    Fix task 6 in logic

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    for c in range(i + 1, 100):
-        if i == 56 and c == 57:
-            print("{:02d}, ".format(i))
-            print("{:02d}, ".format(c))
-        else:
-            print("{:d}, ".format(i))
-            print("{:d}, ".format(c))
+n = 1
+for i in range(0, 8):
+    for c in range(n, 10):
+        print("{:d}{:d}".format(i, c), end=", ")
+    n = n + 1
+exit("89")
