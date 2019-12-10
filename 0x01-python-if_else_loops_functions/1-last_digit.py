7fe25a8c4c833725043bd3b3618e44d729edc2f5
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-10 19:22:07 +0000

    Fix task 1 change of logic and indentation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,12 +8,12 @@ if number < 0:
     last = (positive % 10) * -1
 else:
     last = positive % 10
-if (last < 0 and last < 5) or (last <= 5 and last != 0):
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0\n"
+if last > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5\n"
           .format(number, last))
 elif last == 0:
     print("Last digit of {:d} is {:d} and is 0\n"
           .format(number, last))
-elif last > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5\n"
+elif (last < 0 and last < 5) or (last <= 5 and last != 0):
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0\n"
           .format(number, last))
