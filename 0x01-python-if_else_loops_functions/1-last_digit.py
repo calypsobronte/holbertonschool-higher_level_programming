e7faba34265e3dc1d14c8908c5b7ebe45d887362
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-10 18:56:25 +0000

    Fix task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 last = number % 10
 if last < 0:
     last = last * -1
-if last < 6 and last != 0:
+if last < 6:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0\n".format(number,last))
 elif last == 0:
     print("Last digit of {:d} is {:d} and is 0\n".format(number, last))
