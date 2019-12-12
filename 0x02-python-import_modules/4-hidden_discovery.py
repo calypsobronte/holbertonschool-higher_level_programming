177857f780a6c45d878f09b9b7c40297f00aea8d
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-12 22:27:25 +0000

    add file task 4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+# Si es igual a __ no lo imprima
+if __name__ == "__main__":
+    import hidden_4
+    letter = dir(hidden_4)
+    for i in letter:
+        if i[:2] != "__":
+            print(i)
