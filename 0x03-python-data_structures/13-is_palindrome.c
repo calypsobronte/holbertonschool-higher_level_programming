1d00d28132c920193bf9d76d473ac82e766cb2ff
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-17 21:52:07 +0000

    Add task 13.  Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+int is_palindrome(listint_t **head)
+{
+int len = 0, i;
+if (head == NULL || *head == NULL || (*head)->next == NULL)
+{
+return (1);
+}
+while (head[len] != NULL)
+{
+len++;
+}
+for (i = 0; i < len; i++)
+{
+if ((*head[i]).n != (*head[len - 1 - i]).n)
+{
+return (1);
+}
+}
+return (1);
+}
