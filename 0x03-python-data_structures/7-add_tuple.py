b98cc8e604ed12d3158b5db38b2defa109d8c1f9
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-17 19:21:28 +0000

    Add task  a function that adds 2 tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    num1 = 0
+    num2 = 0
+    if len(tuple_a) >= 2:
+        num1 = tuple_a[0]
+        num2 = tuple_a[1]
+    elif len(tuple_a) == 1:
+        num1 = tuple_a[0]
+    else:
+        num1 = 0
+        num2 = 0
+
+    if len(tuple_b) >= 2:
+        num1 += tuple_b[0]
+        num2 += tuple_b[1]
+    elif len(tuple_b) == 1:
+        num1 += tuple_b[0]
+        num2 += 0
+    else:
+        num1 += 0
+        num2 += 0
+    return (num1, num2)
