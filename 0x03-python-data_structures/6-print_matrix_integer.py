5602a0edb85648124650eb6f46a720c479f58586
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-17 18:31:56 +0000

    Add task 6 function that prints a matrix of integers.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end='')
+            if j + 1 != len(matrix[j]):
+                print(" ", end='')
+        print('')
