c49fb0fc59b0b0ae47bf052e8245ff2cdc921842
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-19 20:15:08 +0000

    Fix task 1 in E501 line too long (86 > 79 characters

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    [(new_list.append(x) if x != search else new_list.append(replace)) for x in my_list]
+    [(new_list.append(x) if x != search else new_list.append(replace))
+        for x in my_list]
     return new_list
