c0f23f5082b73890f372994d55e88a8680463083
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-20 03:39:46 +0000

    add  task 14. squared by using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda num: list(map(lambda num1: num1 ** 2, num)), matrix))
+    return list(map(lambda num: list(map(
+        lambda num1: num1 ** 2, num)), matrix))
