bd3282456c661f596c2f242c8f4841fc6b61bdef
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-20 01:06:23 +0000

    Add task 4. only differents returns a set of all elemnts

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    result = []
+    for element in set_2:
+        if element not in set_1:
+            result.append(element)
+    return result
