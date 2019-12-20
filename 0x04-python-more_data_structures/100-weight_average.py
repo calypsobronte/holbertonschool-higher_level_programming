511df30db2b1cf46b23b5f7c9c32a8ce425f76ad
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-20 03:33:44 +0000

    add  task 13. weighted average of all integers tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    num1 = 0
+    num2 = 0
+    temp = 0
+    for num in range(len(my_list)):
+        num1 += my_list[num][0] * my_list[num][1]
+        num2 += my_list[num][1]
+    return num1 / num2
