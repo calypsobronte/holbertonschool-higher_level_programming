0d521652148b86f1ac2428376760137a8e373687
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-20 02:37:10 +0000

    add task 9. multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {dictionary: a_dictionary[dictionary] * 2 for dictionary in a_dictionary}
+    return {dictionary: a_dictionary[dictionary] * 2
+            for dictionary in a_dictionary}
