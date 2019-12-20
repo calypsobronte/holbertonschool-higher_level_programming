7c139b36448fa6640af35ef6ef035fb04dbd2ceb
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-20 01:11:44 +0000

    Fix task 4 in difference in lists

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    result = []
-    for element in set_2:
-        if element not in set_1:
-            result.append(element)
-    return result
+    return set_1.symmetric_difference(set_2)
