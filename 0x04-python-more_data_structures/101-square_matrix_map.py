2cc4682bf018acdcf170a3e683fa32c05756a116
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-20 03:42:46 +0000

    Fix task requeriment for most characters Change num, num1 for n1, n2

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda num: list(map(
-        lambda num1: num1 ** 2, num)), matrix))
+    return list(map(lambda n1: list(map(lambda n2: n2 ** 2, n1)), matrix))
