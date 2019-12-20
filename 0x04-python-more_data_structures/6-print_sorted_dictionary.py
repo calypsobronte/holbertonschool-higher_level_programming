f9861a8681a5293dc3a6777b0252925093d579f9
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-20 02:19:55 +0000

    add task 6. print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for result_keys in sorted(a_dictionary.keys()):
+        print("{}: {}".format(result_keys, a_dictionary[result_keys]))
