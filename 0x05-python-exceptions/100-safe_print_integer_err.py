876f4dd79e151f02e1b3db1186682ff157ef5598
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-09 02:50:00 +0000

    7. Safe integer print with error message #advanced

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as msg_error:
+        print("Exception: {}".format(msg_error), file=sys.stderr)
+        return False
+    except TypeError as msg_error:
+        print("Exception: {}".format(msg_error), file=sys.stderr)
+        return False
+    return True
