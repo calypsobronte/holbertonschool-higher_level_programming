723e071e3de6e89ab5062a03c59fcf314da5d29c
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-09 02:58:58 +0000

    8. Safe function #advanced

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(args[0], args[1])
+    except ValueError as msg_error:
+        print("Exception: {}".format(msg_error), file=sys.stderr)
+        return(None)
+    except TypeError as msg_error:
+        print("Exception: {}".format(msg_error), file=sys.stderr)
+        return None
+    except ZeroDivisionError as msg_error:
+        print("Exception: {}".format(msg_error), file=sys.stderr)
+        return None
+    except IndexError as msg_error:
+        print("Exception: {}".format(msg_error), file=sys.stderr)
+        return None
+    return result
