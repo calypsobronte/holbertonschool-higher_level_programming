7dee589ac75e604f1a3497f96cf28cddc3797c14
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-09 02:41:21 +0000

    4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            divide = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            divide = 0
+            print("division by 0")
+        except TypeError:
+            divide = 0
+            print("wrong type")
+        except IndexError:
+            divide = 0
+            print("out of range")
+        finally:
+            new_list.append(divide)
+    return new_list
