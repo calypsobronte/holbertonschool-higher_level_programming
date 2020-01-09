8e4ef4dd6381743b6dedaa665b33bfeb2ba35580
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-09 02:35:36 +0000

    3. Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    divide = None
+    try:
+        divide = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        if divide is not None:
+            print("Inside result: {:.1f}".format(divide))
+        else:
+            print("Inside result: {}".format(divide))
+    return divide
