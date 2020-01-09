982ba961a6fb349ef9e1c1f908b319f5db5fb087
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-09 03:05:50 +0000

    Fix task  output 8. Safe function #advanced

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(args[0], args[1])
+        result = fct(*args)
         return result
     except ValueError as msg_error:
         print("Exception: {}".format(msg_error), file=sys.stderr)
