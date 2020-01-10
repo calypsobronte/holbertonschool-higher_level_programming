013eb20bb45f7d28c669941280f1d6b3634fa8d7
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-10 01:40:41 +0000

    Fix task 4 attibute private

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,7 +9,7 @@ class Square:
         return self.__size
 
     @size.setter
-    def __init__(self, size=0):
+    def size(self, size=0):
         if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif (size < 0):
