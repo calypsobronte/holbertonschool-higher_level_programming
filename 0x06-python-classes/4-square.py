4811eeebb8189e9fd33f636f59c1c613f7b6a15f
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-10 01:38:50 +0000

    4. Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+class Square:
+    """ class square with function of size private attribute """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def __init__(self, size=0):
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
