a75d16c58383fe82109e1333cbac5ee24e065813
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-14 17:35:30 +0000

    add task 4 and test 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def text_indentation(text):
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    else:
+        letter = text
+        band = 0
+        for i in range(len(letter)):
+            if band == 0 and letter[i] == " ":
+                band = 0
+                continue
+            band = 1
+            if (band == 1) and (letter[i] == '?' or letter[i] == '.' or letter[i] == ':'):
+                print(letter[i])
+                print()
+                band = 0
+            else:
+                print(letter[i], end='')
