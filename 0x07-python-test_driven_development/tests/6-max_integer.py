61cbb166fdab0c75a17b6e64cc4af8976364b5a5
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-14 20:24:11 +0000

    add task 6 and test 6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([7, -2, 5]), 7)
+        self.assertEqual(max_integer([7, 10, 5]), 10)
+        self.assertEqual(max_integer([-1, -5, -7]), -1)
+        self.assertEqual(max_integer([7]), 7)
+        self.assertEqual(max_integer([]), None)
