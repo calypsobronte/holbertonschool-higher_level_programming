e2c8cce3b942348e7d36a170099c74ab654c5526
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-15 17:59:25 -0500

    Fix task 9 correcy output in size

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -5,15 +5,17 @@ class Rectangle():
 
     @classmethod
     def square(cls, size=0):
-        return cls(width=size, height=size)
+        return cls(size, size)
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1, Rectangle):
+        if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
+        if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        return (rect_2 if rect_2.area() > rect_1.area() else rect_1)
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
 
     def __init__(self, width=0, height=0):
         self.height = height
