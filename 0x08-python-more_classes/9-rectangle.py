3e1a381fb822fada6c122af7fdaf21f24c332d09
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-15 17:47:11 -0500

    Fix task 9 correcy output in size

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -63,6 +63,5 @@ class Rectangle():
             raise TypeError("rect_2 must be an instance of Rectangle")
         return (rect_2 if rect_2.area() > rect_1.area() else rect_1)
 
-    @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return Rectangle(size, size)
