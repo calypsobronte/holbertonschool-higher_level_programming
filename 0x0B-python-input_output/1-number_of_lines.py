d6e744d0e57d545e95506fd7b28f66387655062a
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-21 22:04:14 +0000

    add files task 1. Number of lines

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def number_of_lines(filename=""):
+    open_file = open(filename, 'r', encoding='utf-8')
+    cont_line = 0
+    for lines in open_file:
+        cont_line += 1
+    return cont_line
