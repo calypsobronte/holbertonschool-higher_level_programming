0b3a325ea215a140b8bbf000c96bad035bad5e94
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-21 21:16:14 +0000

    add files task 0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def read_file(filename=""):
+    """ Read from a text file """
+    open_file = open(filename, 'r', encoding='utf-8')
+    read_file2 = open_file.read()
+    print(read_file2)
+    open_file.close()
