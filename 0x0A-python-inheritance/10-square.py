6c65a6d896af187e4a0b99e7615a466b2ad385a8
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-21 05:39:00 +0000

    Fix coming back, since the check did not take the solution

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,6 +6,3 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     def __init__(self, size):
         super().__init__(size, size)
-
-    def area(self):
-        return (self.__size * self.__size)
