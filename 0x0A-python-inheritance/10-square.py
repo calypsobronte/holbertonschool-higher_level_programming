7d23e7a06694b8ada78e3f1831ccc1be62a28541
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-21 05:37:55 +0000

    Fix task 10 for new line also add functions that fix my output

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,8 +7,5 @@ class Square(Rectangle):
     def __init__(self, size):
         super().__init__(size, size)
 
-    def __str__(self):
-            return super().__str__()
-
     def area(self):
         return (self.__size * self.__size)
