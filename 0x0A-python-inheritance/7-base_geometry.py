432b1fc2481874dd59458cbde453749f669c9709
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-21 04:53:31 +0000

    add file task 7. Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+class BaseGeometry:
+    """ task 6 """
+    def area(self):
+        raise Exception("area() is not implemented")
+    """ task 7 """
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
