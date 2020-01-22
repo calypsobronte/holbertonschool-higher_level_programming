1289fd6cf56e615f085170273c6898d721873390
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-22 05:00:02 +0000

    add task 12. Student to JSON with filter

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        array = {}
+        if attrs is None:
+            return self.__dict__
+        else:
+            for cont in attrs:
+                if cont in self.__dict__:
+                    array[cont] = self.__dict__[cont]
+
+        return array
