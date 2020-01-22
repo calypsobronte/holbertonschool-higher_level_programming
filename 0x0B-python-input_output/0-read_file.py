0564d985a9af6321710f1acfee1a740b1706e549
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-22 02:44:32 +0000

    Fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+This is module 0-read_file
+
+This module contains one function `read_file()`
+"""
+
+
 def read_file(filename=""):
-    """ Read from a text file """
+    """
+    function that reads a text file (UTF8) and prints it to stdout
+
+    Args:
+    filename
+    """
     open_file = open(filename, 'r', encoding='utf-8')
     read_file2 = open_file.read()
     print(read_file2)
