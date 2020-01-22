21487be484066309a6e4f7a61d6be1cd43e860ae
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-22 05:15:11 +0000

    add task advanced

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+class MyInt(int):
+    def __eq__(self, num):
+        if (self.real == num):
+            return False
+        return True
+
+    def __ne__(self, num):
+        if(self.real != num):
+            return False
+        return True
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def add_attribute(cla, attri, value):
+    if not hasattr(cla, '__dict__'):
+        raise TypeError("can't add new attribute")
+    setattr(cla, attri, value)
