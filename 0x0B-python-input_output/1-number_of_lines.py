87c4f9e8990bd6463fcf81fe92b8fb34c2d4ddba
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-22 03:04:26 +0000

    No puede ser que esto de asi

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
+    """
     open_file = open(filename, 'r', encoding='utf-8')
     cont_line = 0
     for lines in open_file:
         cont_line += 1
-    return cont_line
+    return cont_line """
+
+    with open(filename, 'r', encoding='utf-8') as target:
+        cont_line = 0
+        for lines in target:
+            cont_line += 1
+        return cont_line
