cefc541c5c2a2670d747bb260f56ba6a69f65318
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-22 04:24:47 +0000

    add task 9. Load, add, save

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import sys
+
+
+""" import of moduls """
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+if __name__ == "__main__":
+
+    try:
+        file_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        file_list = []
+        file_list.extend(sys.argv[1:])
+        save_to_json_file(file_list, "add_item.json")
