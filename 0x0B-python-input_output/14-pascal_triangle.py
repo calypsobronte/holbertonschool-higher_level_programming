d34d73c76fdcc26e076591575516246ff6d9fb88
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-22 05:32:39 +0000

    add task 14. Pascal's Triangle

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    array = []
+    array.append([1, ])
+    for i in range(0, n - 1):
+        array_size = [1, ]
+        for j in range(0, len(array[-1]) - 1):
+            array_size.append(array[-1][j] + array[-1][j + 1])
+        array_size.append(1)
+        array.append(array_size)
+    return array
