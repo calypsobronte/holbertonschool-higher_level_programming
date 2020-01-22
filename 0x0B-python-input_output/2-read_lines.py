c8a0181e089c1659db50cb2e3f451d341bd455ad
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-22 03:24:33 +0000

    add task 2. Read n lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -5,4 +5,4 @@ def read_lines(filename="", nb_lines=0):
             print(target.read(), end="")
         else:
             for line in range(nb_lines):
-                print(target.read(), end="")
+                print(target.readline(), end="")
