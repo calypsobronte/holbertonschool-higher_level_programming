634ffa1b314b8e88aee691137515fcea86e0dfe4
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-03-29 02:53:46 +0000

    [task 2] 2. Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+
+    connect_db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+
+    cursor = connect_db.cursor()
+    cursor.execute("SELECT * FROM states WHERE BINARY name='{}'".format(argv[4]))
+    return_list = cursor.fetchall()
+    for row in return_list:
+        print(row)
+    cursor.close()
+    connect_db.close()
