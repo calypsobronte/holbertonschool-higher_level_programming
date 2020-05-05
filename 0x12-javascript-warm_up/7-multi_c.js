db8225516df3611c36e9dc2ba0d0c4a03a33022a
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-05 23:06:38 +0000

    I love c script loop

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+if (parseInt(process.argv[2]) === true) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) { console.log('C is fun'); }
+}
