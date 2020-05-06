be310d64d462ac94ec5f9c70e05eeff51bad10a0
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-06 00:12:23 +0000

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const number = parseInt(process.argv[2]);
+if (Number.isNaN(number)) { 
+    console.log('Missing size'); 
+} 
+for (let i = 0; i < number; i++) { 
+    console.log('X'.repeat(number)); 
+}
+
