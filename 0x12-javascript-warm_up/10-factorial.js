6ec821dec35beffe261a12d808683b5d6276f737
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-06 00:35:51 +0000

    Fix style fact

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const fact = parseInt(process.argv[2])
-function factorial(number){
-    if (isNaN(number) || number <= 1){
-        return 1
-    }else {
-        return number * factorial(number - 1)
-    }
+const fact = parseInt(process.argv[2]);
+function factorial (number) {
+  if (isNaN(number) || number <= 1) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
 }
-console.log(factorial(fact))
+console.log(factorial(fact));
