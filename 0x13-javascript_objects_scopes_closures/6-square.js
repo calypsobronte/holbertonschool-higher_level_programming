f9f7fa9ea9d4343b86db83787323e4a1055ab8fa
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-13 03:31:11 +0000

    Fix logic in square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square') {
   charPrint (c) {
-    super.print(c);
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
   }
 };
