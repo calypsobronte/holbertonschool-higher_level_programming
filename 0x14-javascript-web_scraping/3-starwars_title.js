a7e088adb359f36a74ca78807344f52f3eb25fc6
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-13 04:58:50 +0000

    Fix style

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-
-const request = require('request');
-const api_file = 'https://swapi-api.hbtn.io/api/films/';
-
-request(api_file + process.argv[2], (error, response, body) => {
-  if (error === null) {
-    const resp = JSON.parse(body);
-    console.log(resp.title);
-  }
-  if (error) {
-    console.log(error);
+const fs = require('request');
+fs.get('http://swapi.co/api/films/' + process.argv[2] + '/', function (error, response, body) {
+  if (error) throw error;
+  else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
   }
 });
