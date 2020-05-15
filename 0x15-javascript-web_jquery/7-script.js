ca9585694d0d09d0574c3e90b3a4237bd986f04c
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-15 00:35:27 +0000

    Fix api

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,1 @@
-$.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', (data) => $('DIV#character').text(data.name););
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', (data) => $('DIV#character').text(data.name);, "json");
